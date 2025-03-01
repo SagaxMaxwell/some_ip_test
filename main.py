7dbb9838b657eb38f81351cc52887a1cc117f468
Maxwell <sagax.maxwell@gmail.com>
2025-03-01 17:44:25 +0800

    Updated README.md.

--- main.py
+++ main.py
@@ -1,8 +1,1 @@
-from socket import SocketKind
-
-
-from modules import *
-
-
-if __name__ == "__main__":
-    print((0x1E63).to_bytes(4, "big"), int(0x1E63, 16))
+pass
--- modules/__init__.py
+++
@@ -1,3 +0,0 @@
-from .packet import Packet
-from .consumer import Consumer
-from .producer import Producer
